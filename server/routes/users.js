20635d109f412623637c7e7b85edd7e066d1067d
Jay <julesmontojo1@mgmail.com>
2022-10-10 11:08:23 +0900

    feat: implement find user by userId or username

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
-router.get('/', (req, res) => {
-  res.status(200).send('users route');
+// router.get('/', async (req, res) => {
+//   return res.status(200).json('users route')
+// });
+
+router.get('/', async (req, res) => {
+  const { userId, username } = req.query;
+
+  try {
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username });
+
+    const { password, updatedAt, ...publicData } = user._doc;
+    return res.status(200).json(publicData);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 module.exports = router;
