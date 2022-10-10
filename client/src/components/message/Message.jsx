ff4913a3500ffab5379c03d502d862bfb7a67880
Jay <julesmontojo1@mgmail.com>
2022-10-11 00:11:46 +0900

    feat: update stylings for messeges

--- client/src/components/message/Message.jsx
+++ client/src/components/message/Message.jsx
@@ -3,14 +3,14 @@ import { format } from 'timeago.js';
 
 export default function Message({ message, currentUser }) {
   return (
-    <div className={ currentUser ? 'message currentUser' : 'message'}>
-      <div className="message-top">
-        <img src="" alt="" className="message-img" />
-        <p className="message-text">
+    <div className={ currentUser ? 'message owned' : 'message'}>
+      <div className="message-main-container">
+        <img src="" alt="" className="message-avatar" />
+        <p className="message-content">
           { message.content }
         </p>
       </div>
-      <div className="message-bottom">
+      <div className="message-timestamp" >
         { format(message.createdAt) }
       </div>
     </div>
