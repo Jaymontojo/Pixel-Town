2f10308c21f85fa5cf73f3695cbca852fc0ecd0d
Jay <julesmontojo1@mgmail.com>
2022-10-10 11:07:34 +0900

    fix: Fix bug in conversations route

--- server/routes/conversations.js
+++ server/routes/conversations.js
@@ -20,9 +20,8 @@ router.get('/:userId', async (req, res) => {
   const { userId } = req.params;
   try {
     const conversations = await Conversation.find({
-      members: { $in:[userId] }
+      participants: { $in:[userId] }
     });
-    if(!conversatons) return res.status(404).json("no conversations found");
     return res.status(200).json(conversations);
   } catch (err) {
     return res.status(500).json(err);
