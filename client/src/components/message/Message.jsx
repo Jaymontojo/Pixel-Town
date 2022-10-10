d851cb716d84e27242d9e379e3b6d96d843e20bc
Jay <julesmontojo1@mgmail.com>
2022-10-10 12:25:26 +0900

    feat: implement dynamic data in Message.jsx

--- client/src/components/message/Message.jsx
+++ client/src/components/message/Message.jsx
@@ -1,15 +1,18 @@
 import './message.css';
+import { format } from 'timeago.js';
 
-export default function Message({ currentUser }) {
+export default function Message({ message, currentUser }) {
   return (
     <div className={ currentUser ? 'message currentUser' : 'message'}>
       <div className="message-top">
         <img src="" alt="" className="message-img" />
-        <p className="message-text">ate mode 100644 client/src/pages/messenger/Messenger.jsx
- create mode 100644 client/src/pages/messenger/messenger.css
-jaymontojo@Jays-Mac-mini chat-app % git cli</p>
+        <p className="message-text">
+          { message.content }
+        </p>
       </div>
-      <div className="message-bottom"> 1 hour ago</div>
+      <div className="message-bottom">
+        { format(message.createdAt) }
+      </div>
     </div>
   )
 }
